2050293ddc21e1e87fca5eb9643d64d53ab72efa
kiarash <kiarashz@hotmail.com>
2025-01-10 18:05:46 +1100

    Add vars to switch easily between workspaces.

--- docker/main.tf
+++ docker/main.tf
@@ -1,25 +1,28 @@
 resource "docker_image" "nginx" {
- name = "nginx:latest"
-  
+  name         = "nginx:latest"
+  keep_locally = true
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 resource "docker_container" "nginx1" {
- image = docker_image.nginx.image_id
- name  = "${var.cprefix}-nginx1"
- network_mode = "bridge"
- ports {
-   internal = 80
-   external = 80
- }
+  image        = docker_image.nginx.image_id
+  name         = "${var.cprefix}-nginx1"
+  network_mode = "bridge"
+  ports {
+    internal = 80
+    external = var.nginx1_port
+  }
 }
 
 resource "docker_container" "nginx2" {
- image = docker_image.nginx.image_id
- name  = "${var.cprefix}-nginx2"
- network_mode = "bridge"
- ports {
-   internal = 80
-   external = 8080
- }
+  image        = docker_image.nginx.image_id
+  name         = "${var.cprefix}-nginx2"
+  network_mode = "bridge"
+  ports {
+    internal = 80
+    external = var.nginx2_port
+  }
 }
 
--- docker/variables.tf
+++ docker/variables.tf
@@ -2,3 +2,15 @@ variable "cprefix" {
   description = "Prefix for the name of the container to make sure different dev/test environments don't clash"
   default     = "dev"
 }
+
+variable "nginx1_port" {
+  description = "Port to expose on the host for the first nginx container"
+  default     = 80
+  
+}
+
+variable "nginx2_port" {
+  description = "Port to expose on the host for the first nginx container"
+  default     = 8080
+  
+}
